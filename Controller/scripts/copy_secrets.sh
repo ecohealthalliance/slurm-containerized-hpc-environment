ecfeb6f4187176c5df100266bf9c3be39d961785
EHA T.05.2023 <andiespirado@gmail.com>
2023-08-23 17:06:11 -0400

    added munge key copy

--- Controller/scripts/copy_secrets.sh
+++ Controller/scripts/copy_secrets.sh
@@ -3,6 +3,6 @@ set -e
 
 cp /home/worker/worker-secret.tar.gz /.secret/worker-secret.tar.gz
   cp /home/worker/setup-worker-ssh.sh /.secret/setup-worker-ssh.sh
-#   cp /etc/munge/munge.key /.secret/munge.key
+  cp /etc/munge/munge.key /.secret/munge.key
   rm -f /home/worker/worker-secret.tar.gz
   rm -f /home/worker/setup-worker-ssh.sh
